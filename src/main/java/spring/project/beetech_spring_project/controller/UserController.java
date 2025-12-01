5b8dbb10b6783da3c414c7d3a0adb957ac82adae
nhatminhdo-217 <nhatminh21072003@gmail.com>
2025-12-01 11:45:06 +0700

    update validation

--- src/main/java/spring/project/beetech_spring_project/controller/UserController.java
+++ src/main/java/spring/project/beetech_spring_project/controller/UserController.java
@@ -1,6 +1,8 @@
 package spring.project.beetech_spring_project.controller;
 
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,8 +14,23 @@ import java.time.LocalDate;
 public class UserController {
 
     @PostMapping("/user")
-    public ResponseEntity<?> myPostMapping(@RequestBody User user) {
+    public ResponseEntity<?> myPostMapping(
+            @RequestBody @Valid User user,
+            BindingResult bindingResult
+    ) {
 
-        return ResponseEntity.ok().build();
+        if (LocalDate.now().getYear() - user.getDob().getYear() < 16) {
+            return ResponseEntity.badRequest().body("User must be greater than 16 years old to register");
+        }
+
+        if (user.getGender() != 1 && user.getGender() != 2) {
+            return ResponseEntity.badRequest().body("Gender must be 1 or 2");
+        }
+
+        if (bindingResult.hasErrors()) {
+            return ResponseEntity.badRequest().body(bindingResult.getAllErrors());
+        }
+
+        return ResponseEntity.ok().body(user);
     }
 }
--- src/main/java/spring/project/beetech_spring_project/entity/User.java
+++ src/main/java/spring/project/beetech_spring_project/entity/User.java
@@ -1,5 +1,7 @@
 package spring.project.beetech_spring_project.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,9 +14,22 @@ import java.time.LocalDate;
 @NoArgsConstructor
 @AllArgsConstructor
 public class User {
+    @NotEmpty(message = "Please enter full name")
     private String fullName;
+
+    @NotEmpty(message = "Please enter email")
+    @Email(regexp = "^\\S+@\\S+\\.\\S+$", message = "Your email do not match the email format. Eg: abc@example.com")
     private String email;
+
+    @NotEmpty(message = "Please enter password")
+    @Size(min = 8, message = "Password must have at least 8 character")
     private String password;
+
+    @NotNull(message = "Please enter date of birth")
+    @JsonFormat(pattern = "dd/MM/yyyy")
+    @Past(message = "Date ov birth must be in past")
     private LocalDate dob;
-    private Boolean gender;
+
+    @NotNull(message = "Please enter gender(1-male, 2-female)")
+    private int gender;
 }
