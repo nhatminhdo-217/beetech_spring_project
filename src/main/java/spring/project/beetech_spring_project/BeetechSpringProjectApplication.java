985f7d8d7e3a198db12403d8c46c83340899b3c4
nhatminhdo-217 <nhatminh21072003@gmail.com>
2025-12-01 09:01:18 +0700

    init commit

---
+++ src/main/java/spring/project/beetech_spring_project/BeetechSpringProjectApplication.java
@@ -0,0 +1,13 @@
+package spring.project.beetech_spring_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeetechSpringProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeetechSpringProjectApplication.class, args);
+    }
+
+}
